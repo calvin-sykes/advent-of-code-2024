05e2a71a8b36f48e8ee1a651fc45816f35fa5cfe
Calvin Sykes <Calvin.Sykes@soton.ac.uk>
2024-12-01 22:11:55 +0000

    add test on example for part2

--- template.py
+++ template.py
@@ -7,4 +7,5 @@ def day$$_part2(filename):
 if __name__ == "__main__":
     print("Part 1 example", day$$_part1("input/day$$_example.txt"))
     print("Part 1", day$$_part1("input/day$$.txt"))
+    print("Part 2 example", day$$_part2("input/day$$_example.txt"))
     print("Part 2", day$$_part2("input/day$$.txt"))
