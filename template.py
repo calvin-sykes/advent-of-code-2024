4b44c1f6fc532f54c677e10caea60d764ad16793
Calvin Sykes <Calvin.Sykes@soton.ac.uk>
2024-12-01 21:51:58 +0000

    create script template

---
+++ template.py
@@ -0,0 +1,10 @@
+def day$$_part1(filename):
+    pass
+
+def day$$_part2(filename):
+    pass
+
+if __name__ == "__main__":
+    print("Part 1 example", day$$_part1("input/day$$_example.txt"))
+    print("Part 1", day$$_part1("input/day$$.txt"))
+    print("Part 2", day$$_part2("input/day$$.txt"))
