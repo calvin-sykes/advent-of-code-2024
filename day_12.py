77e2a0e6bca0f8c20c71b86bf5e086ea6a3bb3ec
Calvin Sykes <Calvin.Sykes@soton.ac.uk>
2024-12-12 22:02:07 +0000

    simplify day 12

--- day_12.py
+++ day_12.py
@@ -7,55 +7,21 @@ def find_regions(lines):
 
     for row, l in enumerate(lines):
         for col, c in enumerate(l):
-            for base in candidates:
-                if (row, col) in candidates[base]:
-                    regions[base].add((row, col))
-                    candidates[base].remove((row, col))
-                    break
-            else:
+            try:
+                base, *others = {base for base in candidates if (row, col) in candidates[base]}
+            except ValueError:
                 base = (row, col, c)
                 regions[base].add((row, col))
+            else:
+                regions[base].add((row, col))
+                candidates[base].remove((row, col))
+                for other in others:
+                    regions[base].update(regions.pop(other))
+                    candidates[base].update(candidates.pop(other))
             for d in range(4):
                 nr, nc = row + D.dy[d], col + D.dx[d]
                 if bounds([nr, nc], [(0, h), (0, w)]) and lines[nr][nc] == c:
                     candidates[base].add((nr, nc))
-    
-    # Check for regions that actually join up
-    joins = coll.defaultdict(set)
-    for base1, base2 in it.combinations(regions, 2):
-        if base1 == base2:
-            continue
-        if base1[2] != base2[2]:
-            continue
-        for (pr, pc) in regions[base1]:
-            for d in range(4):
-                nr, nc = pr + D.dy[d], pc + D.dx[d]
-                if (nr, nc) in regions[base2]:
-                    joins[base1].add(base2)
-
-    merged_joins = dict()
-    for joiner, joinee in joins.items():
-        for base_join in merged_joins:
-            if joiner in merged_joins[base_join]:
-                merged_joins[base_join].update(joinee)
-                break
-        else:
-            merged_joins[joiner] = joinee.copy()
-
-    joined_to = dict()
-    for joiner, joinees in merged_joins.items():
-        for joinee in joinees:
-            try:
-                regions[joiner].update(regions.pop(joinee))
-                joined_to[joinee] = joiner
-            except:
-                while joinee not in regions:
-                    joinee = joined_to[joinee]
-                if joiner == joinee:
-                    continue
-                regions[joiner].update(regions.pop(joinee))
-                joined_to[joinee] = joiner
-
     return regions
 
 def day12_part1(filename):
@@ -98,17 +64,14 @@ def day12_part2(filename):
                     if lines[nr][nc] == l:
                         ngb.add((D.dy[d], D.dx[d]))
             n_ngb = len(ngb)
-            if n_ngb == 4:
-                # + shape
+            if n_ngb == 4: # + shape
                 corners = 4
                 for (cr, cc) in {(1, 1), (1, -1), (-1, 1), (-1, -1)}:
                     nr, nc = pr + cr, pc + cc
                     if bounds([nr, nc], [(0, h), (0, w)]):
                         if (nr, nc) in regions[base]:
                             corners -= 1
-                    assert corners >= 0
-            elif n_ngb == 3:
-                # T shape
+            elif n_ngb == 3: # T shape
                 corners = 2
                 c1, c2, c3 = ngb
                 for (c1, c2) in it.combinations(ngb, 2):
@@ -119,23 +82,21 @@ def day12_part2(filename):
                     if bounds([nr, nc], [(0, h), (0, w)]):
                         if (nr, nc) in regions[base]:
                             corners -= 1
-            elif n_ngb == 2:
+            elif n_ngb == 2: # either straight edge, or L shape
                 if (ngb == {(0, 1), (0, -1)}) or (ngb == {(1, 0), (-1, 0)}):
-                    corners = 0 # inline
+                    corners = 0
                 else:
                     c1, c2 = ngb
                     cr, cc = c1[0] + c2[0], c1[1] + c2[1]
                     nr, nc = pr + cr, pc + cc
                     if (nr, nc) in regions[base]:
-                        corners = 1 # outer corner only
+                        corners = 1
                     else:
-                        corners = 2 # inner corner
-            elif n_ngb == 1:
-                corners = 2 # peninsula
-            elif n_ngb == 0:
-                corners = 4 # isolated
-            else:
-                raise ValueError
+                        corners = 2
+            elif n_ngb == 1: # peninsula
+                corners = 2
+            else: # isolated
+                corners = 4
             sides += corners
         fences += area * sides
     return fences
